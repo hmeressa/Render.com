0020f7e41c2c4bcae09fb907c12060341ae840fb
hiluf.meressa@ienetworksolutions.com <hmkahsay@gmail.com>
2023-09-08 11:19:07 +0300

    enviroment variables are changed

--- src/config/typeOrm.config.js
+++ src/config/typeOrm.config.js
@@ -7,7 +7,7 @@ const { User } = require("../model");
 module.exports = {
     type: 'postgres',
     host: configs.postgres.host,
-    ssl: { rejectUnauthorized: true },
+    ssl: { rejectUnauthorized: false },
     port: configs.postgres.port,
     username: configs.postgres.userName,
     password: configs.postgres.pswd,
