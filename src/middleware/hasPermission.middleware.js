185b56de7545331dacbf8ce8dbbea22e5dca204d
hiluf.meressa@ienetworksolutions.com <hmkahsay@gmail.com>
2023-09-20 14:22:46 +0300

    permission is reimplemented

---
+++ src/middleware/hasPermission.middleware.js
@@ -0,0 +1,5 @@
+const hasPermission = () => {
+
+}
+
+module.exports = { hasPermission };
--- src/middleware/permission.middleware.js
+++ src/middleware/permission.middleware.js
@@ -1,19 +1,22 @@
 const { ErrorApi } = require("../handler/error.handler");
 
-const permissions = (requiredPermission = []) => {
+const permissions = (requiredPermissions = []) => {
     return (req, res, next) => {
         try {
             const user = req.user;
             if (!user) {
                 return next(new ErrorApi("User is Not Authorized. Please Login first", 401));
             }
+
             if (user?.role?.permission) {
-                const hasRequiredPermission = user.role.permission.some(perm =>
-                    requiredPermission.includes(perm.name)
+                const hasRequiredPermission = requiredPermissions.every(requiredPermission =>
+                    user.role.permission
+                        .map(permission => permission.name)
+                        .includes(requiredPermission)
                 );
 
                 if (!hasRequiredPermission) {
-                    return next(new ErrorApi("You don't have the required permission", 401));
+                    return next(new ErrorApi("You don't have the required permission(s)", 401));
                 }
                 next();
             }
