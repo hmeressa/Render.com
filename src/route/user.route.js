aa001435a946ecdbf3e810c4541060071868f6fa
hiluf.meressa@ienetworksolutions.com <hmkahsay@gmail.com>
2023-09-20 14:44:09 +0300

    permission is reimplemented

--- src/route/user.route.js
+++ src/route/user.route.js
@@ -5,8 +5,8 @@ const { permissions } = require('../middleware/permission.middleware')
 const router = express.Router();
 
 router.route("/")
-    .post(authorize, permissions(['create-user']), userController.createUser)
-    .get(authorize, permissions(['view-user']), userController.getUsers)
+    .post(userController.createUser)
+    .get(userController.getUsers)
 
 router.route('/:id')
     .get(authorize, permissions(['view-user']), userController.getUser)
