daca31041ddcf5c3d9383a6f7807cb745a486b0d
hiluf.meressa@ienetworksolutions.com <hmkahsay@gmail.com>
2024-04-05 17:07:50 +0300

    ssl is add

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -5,7 +5,6 @@ const { RedisInstance } = require('../config/redis.config');
 
 const createUser = async (req, res, next) => {
     const result = await userService.createUser(req.body);
-    // const redisStorage = RedisInstance.set("RedisStorage", result);
     res.status(200).json({
         status: result,
         statusCode: 200,
@@ -27,11 +26,7 @@ const getUser = async (req, res, next) => {
 
 const updateUser = async (req, res, next) => {
     const result = await userService.updateUser(req.params.id, req.body);
-    res.status(200).json({
-        statusCode: 200,
-        status: "Success",
-        User: result
-    });
+    res.send(result);
 }
 
 const deleteUser = async (req, res, next) => {
@@ -40,12 +35,7 @@ const deleteUser = async (req, res, next) => {
         return next(new ErrorApi("User not Found", 404))
     }
     await userService.deleteUser(req.params.id);
-
-    res.status(200).json({
-        statusCode: 200,
-        status: "Success",
-        User: result
-    });
+    res.send(result);
 }
 
 module.exports =
